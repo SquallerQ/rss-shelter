8355ba79890830a1c7db7631d94abcaa5c9bba82
squallerQ <john199502031@gmail.com>
2025-11-13 20:09:55 +0100

    refactor: made slider code more concise while preserving functionality

--- js/main.js
+++ js/main.js
@@ -9,204 +9,93 @@ const openModal = initModal();
 initBurgerMenu();
 
 // Slider
-let counterRight = 0;
-let counterLeft = 0;
+const cardsContainer = document.querySelector(".pets__slider-cards");
+const buttonRight = document.querySelector(".pets__slider-arrow--right");
+const buttonLeft = document.querySelector(".pets__slider-arrow--left");
 
-let previousStateOnScreen = [];
-let arrayForLastTwoCards = [];
+let allPetsArray = PETS_ARRAY.map((element) => element);
 
-let prevLeftArray = [];
-let prevRightArray = [];
+let cardsOnScreen = [];
+let historyRight = [];
+let historyLeft = [];
 
-let prevL = false
-let prevR = false
-
-const cardsContainer = document.querySelector('.pets__slider-cards')
-let GlobalArrayCopyForSlider = PETS_ARRAY.map((element) => element);
-
-function renderCards (numberOfSlides, array, clearScreen = false, useExactArray = false) {
-  if (clearScreen === true || useExactArray === true) {
+function renderCards(numberOfSlides, array, clearScreen = false, fromHistory = false) {
+  if (clearScreen === true || fromHistory === true) {
     const cardsFromPage = document.querySelectorAll(".pets__slider-card");
     cardsFromPage.forEach(function (item) {
       item.style.display = "none";
     });
   }
 
-  let newArrayForThreeCards = [];
-  if (useExactArray === false) {
-    newArrayForThreeCards = [];
-    
+  let selectedCards = [];
+  if (fromHistory === false) {
     function getRandomInt(max) {
       return Math.floor(Math.random() * max);
     }
     
     for (let i = 0; i < numberOfSlides; i++) {
       let randomNumber = getRandomInt(array.length);
-      if (!newArrayForThreeCards.includes(array[randomNumber])) {
-        newArrayForThreeCards.push(array[randomNumber]);
+      if (!selectedCards.includes(array[randomNumber])) {
+        selectedCards.push(array[randomNumber]);
       } else {
         i = i - 1;
       }
     }
   } else {
-    newArrayForThreeCards = array;
+    selectedCards = array;
   }
 
-  newArrayForThreeCards = newArrayForThreeCards.sort((a, b) => {
+  selectedCards = selectedCards.sort((a, b) => {
     if (a.name < b.name) return -1;
     if (a.name > b.name) return 1;
     return 0;
   });
 
-  for (let i = 0; i < newArrayForThreeCards.length; i++) {
+  for (let i = 0; i < selectedCards.length; i++) {
     let element = document.createElement('div');
     element.className = 'pets__slider-card';
     element.innerHTML = `
-                  <img src='${newArrayForThreeCards[i].img}' alt='slider image' class='pets__slider-image'>
-                  <p class='pets__slider-text'>${newArrayForThreeCards[i].name}</p>
+                  <img src='${selectedCards[i].img}' alt='slider image' class='pets__slider-image'>
+                  <p class='pets__slider-text'>${selectedCards[i].name}</p>
                   <button class='pets__slider-button'>Learn more</button>`;
     element.addEventListener('click', () => {
-      openModal(newArrayForThreeCards[i], "main");
+      openModal(selectedCards[i], "main");
     });
     cardsContainer.append(element);
   }
 
-  if (useExactArray === false && clearScreen === false) {
-    previousStateOnScreen.push(...newArrayForThreeCards);
-    return GlobalArrayCopyForSlider.filter((item) => !newArrayForThreeCards.includes(item));
-  } else if (useExactArray === false && clearScreen === true) {
-    array = array.filter((item) => !newArrayForThreeCards.includes(item));
-    arrayForLastTwoCards.push(...array);
-  }
-}
-// 5 Cards (8 - 3 (on Screen) = 5)
-let modifiedArrayAfterRender = renderCards(3, GlobalArrayCopyForSlider);
-// Array for 2 Cards after click. 8-3-3=2 (3 on Screen) (3 on screen after click)
-
-const buttonRight = document.querySelector('.pets__slider-arrow--right');
-const buttonLeft = document.querySelector('.pets__slider-arrow--left');
-
-function lastThreeCardsRender (arrayWithEightCards, arrayWithFiveCards) {
-  let newThreeCards = arrayWithEightCards.filter((item) => !arrayWithFiveCards.includes(item));
-  let newFiveCards = arrayWithEightCards.filter((item) => !newThreeCards.includes(item));
-
-  let resultArray = []
-  resultArray.push(newThreeCards);
-  resultArray.push(newFiveCards);
-
-  return resultArray;
+  return selectedCards;
 }
 
-buttonRight.addEventListener('click', function () {
-  let twoArrays = lastThreeCardsRender(GlobalArrayCopyForSlider, modifiedArrayAfterRender);
-  
-  if (counterLeft == 0 && counterRight == 0) {
-    renderCards(3, twoArrays[1], true);
+function getAvailableCards() {
+  return allPetsArray.filter((item) => !cardsOnScreen.includes(item));
+}
 
-    previousStateOnScreen = []
-    previousStateOnScreen.push(...twoArrays[0]);
+// Renders cards and updates state simultaneously
+cardsOnScreen = renderCards(3, allPetsArray);
 
-    modifiedArrayAfterRender = [];
-    modifiedArrayAfterRender = twoArrays[0].concat(arrayForLastTwoCards);
-    arrayForLastTwoCards = []
-
+buttonRight.addEventListener("click", function () {
+  if (historyLeft.length > 0) {
+    historyRight.push([...cardsOnScreen]);
+    cardsOnScreen = historyLeft.pop();
+    renderCards(0, cardsOnScreen, true, true);
+  } else {
+    historyRight.push([...cardsOnScreen]);
+    let availableCards = getAvailableCards();
+    // Renders cards and updates state simultaneously
+    cardsOnScreen = renderCards(3, availableCards, true);
   }
-  if (counterLeft > 0 && counterRight == 0) {
-    if (prevR == true) {
-      renderCards(0, prevRightArray, false, true);  
-    }
-    if (prevR == false) {
-      renderCards(0, previousStateOnScreen, false, true);
-      prevRightArray.push(...previousStateOnScreen)
-      previousStateOnScreen = [];
-      previousStateOnScreen.push(...twoArrays[0])
-      prevR = true     
-    }
-  }
-  if (prevR == false && counterLeft == 0 && counterRight > 0) {
-    renderCards(3, twoArrays[1], true);
-
-    previousStateOnScreen = []
-    previousStateOnScreen.push(...twoArrays[0]);
-
-
-    modifiedArrayAfterRender = [];
-    modifiedArrayAfterRender = twoArrays[0].concat(arrayForLastTwoCards);
-    arrayForLastTwoCards = []
-  }
-  if (prevR == true && counterLeft == 0 && counterRight > 0) {
-    modifiedArrayAfterRender = GlobalArrayCopyForSlider.filter((item) => !prevRightArray.includes(item))
-    twoArrays = lastThreeCardsRender(GlobalArrayCopyForSlider, modifiedArrayAfterRender);    
-
-    renderCards(3, twoArrays[1], true);
-    prevLeftArray = [];
-    prevRightArray = [];
-    prevL = false;
-    prevR = false;
-
-    previousStateOnScreen = []
-    previousStateOnScreen.push(...twoArrays[0]);
-
-    modifiedArrayAfterRender = [];
-    modifiedArrayAfterRender = twoArrays[0].concat(arrayForLastTwoCards);
-    arrayForLastTwoCards = []
-  }
-
-  counterRight++;
-  counterLeft = 0;
 });
 
-buttonLeft.addEventListener('click', function () {
-  let twoArrays = lastThreeCardsRender(GlobalArrayCopyForSlider, modifiedArrayAfterRender);
-  if (counterLeft == 0 && counterRight == 0) {
-    renderCards(3, twoArrays[1], true);
-
-    previousStateOnScreen = []
-    previousStateOnScreen.push(...twoArrays[0]);
-
-    modifiedArrayAfterRender = [];
-    modifiedArrayAfterRender = twoArrays[0].concat(arrayForLastTwoCards);
-    arrayForLastTwoCards = []
+buttonLeft.addEventListener("click", function () {
+  if (historyRight.length > 0) {
+    historyLeft.push([...cardsOnScreen]);
+    cardsOnScreen = historyRight.pop();
+    renderCards(0, cardsOnScreen, true, true);
+  } else {
+    historyLeft.push([...cardsOnScreen]);
+    let availableCards = getAvailableCards();
+    // Renders cards and updates state simultaneously
+    cardsOnScreen = renderCards(3, availableCards, true);
   }
-  if (counterRight > 0 && counterLeft == 0) {
-    if (prevL == true) {
-      renderCards(0, prevLeftArray, false, true);
-    }
-    if (prevL == false) {
-      renderCards(0, previousStateOnScreen, false, true);
-      prevLeftArray.push(...previousStateOnScreen)
-      previousStateOnScreen = [];
-      previousStateOnScreen.push(...twoArrays[0])
-      prevL = true
-    }
-  }
-  if (prevL == false && counterRight == 0 && counterLeft > 0) {
-    renderCards(3, twoArrays[1], true);
-
-    previousStateOnScreen = []
-    previousStateOnScreen.push(...twoArrays[0]);
-
-    modifiedArrayAfterRender = [];
-    modifiedArrayAfterRender = twoArrays[0].concat(arrayForLastTwoCards);
-    arrayForLastTwoCards = []
-  }
-  if (prevL == true && counterRight == 0 && counterLeft > 0) {
-    modifiedArrayAfterRender = GlobalArrayCopyForSlider.filter((item) => !prevLeftArray.includes(item))
-    twoArrays = lastThreeCardsRender(GlobalArrayCopyForSlider, modifiedArrayAfterRender);   
-
-    renderCards(3, twoArrays[1], true);
-    prevLeftArray = [];
-    prevRightArray = [];
-    prevL = false;
-    prevR = false;
-
-    previousStateOnScreen = []
-    previousStateOnScreen.push(...twoArrays[0]);
-
-    modifiedArrayAfterRender = [];
-    modifiedArrayAfterRender = twoArrays[0].concat(arrayForLastTwoCards);
-    arrayForLastTwoCards = []
-  }
-    counterLeft++;
-    counterRight = 0;
 });
